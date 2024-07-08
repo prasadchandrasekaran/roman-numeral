fb235b6edc2d9d96267bf0703b63b48896c34d52
prasadchandrasekaran <prasadchandrasekarang@gmail.com>
2024-07-08 10:42:44 -0600

    test changes

--- src/main/java/org/adobe/interview/romannumeral/service/RomanNumeralServiceImpl.java
+++ src/main/java/org/adobe/interview/romannumeral/service/RomanNumeralServiceImpl.java
@@ -13,6 +13,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 @Slf4j
 public class RomanNumeralServiceImpl implements RomanNumeralService {
 
+    private static final long MAX_SMALL_NUMBER = 3999;
+    private static final long MAX_NUMBER = 3999999999L;
+    private static final String TOO_BIG_MESSAGE = "Too big (over 3,999,999,999)";
+
     private final Tracer tracer;
 
     @Autowired
@@ -27,6 +31,9 @@ public class RomanNumeralServiceImpl implements RomanNumeralService {
         log.info("Converting number to Roman numeral: {}", number);
         Span newSpan = tracer.nextSpan().name("convertIntegerToRomanNumeral").start();
         try (Tracer.SpanInScope ws = tracer.withSpan(newSpan.start())) {
+            if (number > MAX_NUMBER) {
+                return TOO_BIG_MESSAGE;
+            }
             String result = toRoman(number);
             log.info("Converted number {} to Roman numeral {}", number, result);
             return result;
@@ -41,29 +48,75 @@ public class RomanNumeralServiceImpl implements RomanNumeralService {
     private String toRoman(long number) {
         Span span = tracer.nextSpan().name("toRoman").start();
         try (Tracer.SpanInScope ws = tracer.withSpan(span.start())) {
-            if (number < 1 || number > 39999999) {
-                log.error("Invalid number: {}. Number out of range (1-39999999)", number);
-                throw new IllegalArgumentException(String.format("number %d out of range (1-39999999)", number));
+            if (number < 1 || number > MAX_NUMBER) {
+                log.error("Invalid number: {}. Number out of range (1-3999999999)", number);
+                throw new IllegalArgumentException(String.format("number %d out of range (1-3999999999)", number));
             }
-            return convert((int) number);
+
+            StringBuilder resultString = new StringBuilder();
+            String numberStr = Long.toString(number);
+            int length = numberStr.length();
+
+            for (int i = 0; i < length; i++) {
+                int currNumber = Character.getNumericValue(numberStr.charAt(length - 1 - i));
+                if (currNumber == 0) continue;
+
+                if (i <= 2) {
+                    resultString.insert(0, processSmallNumber(currNumber, i));
+                } else if (number > MAX_SMALL_NUMBER) {
+                    resultString.insert(0, processLargeNumberVinculum(currNumber, i));
+                } else {
+                    if (i == 3) resultString.insert(0, "M".repeat(currNumber));
+                }
+            }
+
+            return resultString.toString();
         } finally {
             span.end();
         }
     }
 
-    private String convert(int number) {
-        Span span = tracer.nextSpan().name("convert").start();
-        try (Tracer.SpanInScope ws = tracer.withSpan(span.start())) {
-            StringBuilder sb = new StringBuilder();
-            while (number > 0) {
-                int floored = RomanNumeralMappings.ROMAN_NUMERALS.floorKey(number);
-                String numeral = RomanNumeralMappings.ROMAN_NUMERALS.get(floored);
-                sb.append(numeral);
-                number -= floored;
-            }
-            return sb.toString();
-        } finally {
-            span.end();
+    private String processSmallNumber(int currNumber, int position) {
+        String[] romanChars = getRomanCharacters(position);
+        return individualRomanCharConverter(currNumber, romanChars[0], romanChars[1], romanChars[2]);
+    }
+
+    private String processLargeNumberVinculum(int currNumber, int position) {
+        String[] romanChars = getRomanCharacters(position % 3);
+        if ((position == 6 || position == 9) && currNumber < 4) {
+            romanChars[0] = "M";
+        }
+        String currRomanChars = individualRomanCharConverter(currNumber, romanChars[0], romanChars[1], romanChars[2]);
+        String line = position >= 6 ? "\u0305\u0305" : "\u0305";
+        StringBuilder result = new StringBuilder();
+        for (char c : currRomanChars.toCharArray()) {
+            result.append(c).append(line);
+        }
+        return result.toString();
+    }
+
+    private String[] getRomanCharacters(int position) {
+        switch (position) {
+            case 0: return new String[]{"I", "V", "X"};
+            case 1: return new String[]{"X", "L", "C"};
+            case 2: return new String[]{"C", "D", "M"};
+            default: return new String[]{"", "", ""};
+        }
+    }
+
+    private String individualRomanCharConverter(int currNumber, String currSingularChar, String currHalfChar, String currTenChar) {
+        switch (currNumber) {
+            case 0: return "";
+            case 1: return currSingularChar;
+            case 2: return currSingularChar.repeat(2);
+            case 3: return currSingularChar.repeat(3);
+            case 4: return currSingularChar + currHalfChar;
+            case 5: return currHalfChar;
+            case 6: return currHalfChar + currSingularChar;
+            case 7: return currHalfChar + currSingularChar.repeat(2);
+            case 8: return currHalfChar + currSingularChar.repeat(3);
+            case 9: return currSingularChar + currTenChar;
+            default: return "";
         }
     }
 }
--- src/test/groovy/org/adobe/interview/romannumeral/service/RomanNumeralServiceImplSpec.groovy
+++ src/test/groovy/org/adobe/interview/romannumeral/service/RomanNumeralServiceImplSpec.groovy
@@ -1,62 +1,63 @@
-//package org.adobe.interview.romannumeral.service
-//
-//import spock.lang.Specification
-//import spock.lang.Unroll
-//
-///**
-// * Unit tests for RomanNumeralServiceImpl using Spock framework.
-// */
-//class RomanNumeralServiceImplSpec extends Specification {
-//
-//    // Instantiate the service to be tested
-//    def romanNumeralService = new RomanNumeralServiceImpl()
-//
-//    /**
-//     * Tests the conversion of integers to Roman numerals.
-//     * Uses the `@Unroll` annotation to run the test for each set of input and expected output.
-//     *
-//     * The `where` block provides various test cases:
-//     * - Converts integers to their Roman numeral equivalents.
-//     */
-//    @Unroll
-//    def "convertIntegerToRomanNumeral should convert #number to Roman numeral #expected"() {
-//        expect: // Verifies that the result of the conversion matches the expected value
-//        romanNumeralService.convertNumberToRomanNumeral(number) == expected
-//
-//        where: // Provides test data for the conversion
-//        number | expected
-//        1      | "I"             // Tests the conversion of 1 to "I"
-//        4      | "IV"            // Tests the conversion of 4 to "IV"
-//        9      | "IX"            // Tests the conversion of 9 to "IX"
-//        40     | "XL"            // Tests the conversion of 40 to "XL"
-//        90     | "XC"            // Tests the conversion of 90 to "XC"
-//        400    | "CD"            // Tests the conversion of 400 to "CD"
-//        900    | "CM"            // Tests the conversion of 900 to "CM"
-//        1000   | "M"             // Tests the conversion of 1000 to "M"
-//        1987   | "MCMLXXXVII"    // Tests the conversion of 1987 to "MCMLXXXVII"
-//        3999   | "MMMCMXCIX"     // Tests the conversion of 3999 to "MMMCMXCIX"
-//    }
-//
-//    /**
-//     * Tests that an IllegalArgumentException is thrown for numbers outside the valid range (1-3999).
-//     * Uses the `@Unroll` annotation to run the test for each invalid input.
-//     *
-//     * The `where` block provides various out-of-range numbers:
-//     * - Tests invalid numbers to ensure proper exception handling.
-//     */
-//    @Unroll
-//    def "convertIntegerToRomanNumeral should throw IllegalArgumentException for out of range number #number"() {
-//        when: // Executes the conversion method with an invalid input
-//        romanNumeralService.convertNumberToRomanNumeral(number)
-//
-//        then: // Verifies that an IllegalArgumentException is thrown and checks the exception message
-//        def e = thrown(IllegalArgumentException)
-//        e.message == "number $number out of range (1-3999)"
-//
-//        where: // Provides invalid test data
-//        number | _
-//        0      | _ // Tests the conversion of 0, which is out of range
-//        4000   | _ // Tests the conversion of 4000, which is out of range
-//        -1     | _ // Tests the conversion of -1, which is out of range
-//    }
-//}
+package org.adobe.interview.romannumeral.service
+
+import io.micrometer.tracing.Span
+import io.micrometer.tracing.Tracer
+import spock.lang.Specification
+import spock.lang.Unroll
+
+/**
+ * Unit tests for RomanNumeralServiceImpl using Spock framework.
+ */
+class RomanNumeralServiceImplSpec extends Specification {
+
+    def tracer = Mock(Tracer)
+    def span = Mock(Span)
+    def romanNumeralService = new RomanNumeralServiceImpl(tracer)
+
+    def setup() {
+        // Setup mock behavior
+        tracer.nextSpan() >> span
+        span.name(_) >> span
+        span.start() >> span
+        span.end() >> null
+    }
+
+    @Unroll
+    def "convertNumberToRomanNumeral should convert #number to Roman numeral #expected"() {
+        expect:
+        romanNumeralService.convertNumberToRomanNumeral(number) == expected
+
+        where:
+        number       | expected
+        1            | "I"
+        4            | "IV"
+        9            | "IX"
+        40           | "XL"
+        90           | "XC"
+        400          | "CD"
+        900          | "CM"
+        1000         | "M"
+        1987         | "MCMLXXXVII"
+        3999         | "MMMCMXCIX"
+        4000         | "I̅V̅"
+        5000         | "V̅"
+        10000        | "X̅"
+        1000000      | "M̅̅"
+    }
+
+    @Unroll
+    def "convertNumberToRomanNumeral should throw IllegalArgumentException for out of range number #number"() {
+        when:
+        romanNumeralService.convertNumberToRomanNumeral(number)
+
+        then:
+        def e = thrown(IllegalArgumentException)
+        e.message == "number $number out of range (1-3999999999)"
+
+        where:
+        number | _
+        0      | _
+        4000000000 | _
+        -1     | _
+    }
+}
